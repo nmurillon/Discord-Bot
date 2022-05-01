28308cd9d3416a41a34c3b967a597eaecc059058
Nicolas MURILLON <nicolas.murillon@etu.uca.fr>
2022-05-01 23:41:10 +0200

    added express

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 const Discord = require('discord.js');
 const mongoose = require('mongoose');
+const express = require('express')
+const app = express()
 
 /* A bot is a client */
 const client = new Discord.Client({ 
@@ -23,3 +25,10 @@ mongoose.connect(process.env.MONGO_SRV).then(() => {
 });
 
 client.login(process.env.TOKEN);
+
+/* Usefull for Azure App servcices, it needs something listening on port 8080*/
+app.get('/', function (req, res) {
+    res.send('hello world')
+  })
+  
+  app.listen(8080)
